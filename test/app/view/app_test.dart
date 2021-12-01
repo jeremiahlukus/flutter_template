a94785bbd44a035f09c47bb47785d1e6b8c4bbf6
Jeremiah-Parrack <jeremiah.parrack@coxautoinc.com>
2021-12-01 18:25:39 -0500

    Jeremiah-Parrack | it works

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -5,12 +5,13 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
-// Project imports:
-import 'package:flutter_template/app/app.dart';
-import 'package:flutter_template/counter/counter.dart';
 // Package imports:
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+// Project imports:
+import 'package:flutter_template/app/app.dart';
+import 'package:flutter_template/counter/counter.dart';
 
 void main() {
   group('App', () {
