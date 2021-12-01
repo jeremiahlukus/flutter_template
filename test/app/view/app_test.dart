cdbf7fc1b345722524b3b45954a2d12b6faefbaf
Jeremiah-Parrack <jeremiah.parrack@coxautoinc.com>
2021-12-01 18:22:49 -0500

    Jeremiah-Parrack | lint

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -5,16 +5,22 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
+// Project imports:
+import 'package:flutter_template/app/app.dart';
+import 'package:flutter_template/counter/counter.dart';
 // Package imports:
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-// Project imports:
-import 'package:flutter_template/app/app.dart';
-import 'package:flutter_template/counter/counter.dart';
-
 void main() {
   group('App', () {
+    testWidgets('renders CounterPage', (tester) async {
+      await tester.pumpWidget(const ProviderScope(child: App()));
+      await tester.pumpAndSettle();
+      expect(find.byType(CounterPage), findsOneWidget);
+    });
+  });
+  group("App test", () {
     testWidgets('renders CounterPage', (tester) async {
       await tester.pumpWidget(const ProviderScope(child: App()));
       await tester.pumpAndSettle();
