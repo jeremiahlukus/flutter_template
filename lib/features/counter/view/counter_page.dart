8e185de3fa202d4c48f00c75173d9a83c4c831bc
Jeremiah-Parrack <jeremiah.parrack@coxautoinc.com>
2021-12-02 14:15:52 -0500

    Jeremiah-Parrack | fix typo in name, remove block wrapper

--- lib/features/counter/view/counter_page.dart
+++ lib/features/counter/view/counter_page.dart
@@ -13,7 +13,6 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 // Project imports:
 import 'package:flutter_template/app/view/app.dart';
-import 'package:flutter_template/utils/block_warpper.dart';
 
 class CounterPage extends StatelessWidget {
   const CounterPage({Key? key}) : super(key: key);
@@ -29,27 +28,25 @@ class CounterView extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return BlockWrapper(
-      Scaffold(
-        appBar: AppBar(title: const Text('Flutter Template')),
-        body: const Center(child: CounterText()),
-        floatingActionButton: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            FloatingActionButton(
-              tooltip: 'Increment',
-              onPressed: () => ref.read(counterProvider.notifier).increment(),
-              child: const Icon(Icons.add),
-            ),
-            const SizedBox(height: 8),
-            FloatingActionButton(
-              tooltip: 'Decrement',
-              onPressed: () => ref.read(counterProvider.notifier).decrement(),
-              child: const Icon(Icons.remove),
-            ),
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(title: const Text('Flutter Template')),
+      body: const Center(child: CounterText()),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            tooltip: 'Increment',
+            onPressed: () => ref.read(counterProvider.notifier).increment(),
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(height: 8),
+          FloatingActionButton(
+            tooltip: 'Decrement',
+            onPressed: () => ref.read(counterProvider.notifier).decrement(),
+            child: const Icon(Icons.remove),
+          ),
+        ],
       ),
     );
   }
