2be14bbea11b44d7ed8ba79ee97651aa6097e88c
Victor Eronmosele <victoreronmosele@gmail.com>
2022-04-27 19:54:30 +0100

    Add test for SplashPage

---
+++ test/splash/presentation/splash_page_test.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_template/splash/presentation/splash_page.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+void main() {
+  group('SplashPage', () {
+    testWidgets('contains the FontAwesomeIcons.github icon', (tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: SplashPage(),
+        ),
+      );
+
+      await tester.pump(Duration.zero);
+
+      final welcomeTextFinder = find.byWidgetPredicate(
+        (Widget widget) => widget is FaIcon && widget.icon == FontAwesomeIcons.github,
+      );
+
+      expect(welcomeTextFinder, findsOneWidget);
+    });
+
+    testWidgets('contains the LinearProgressIndicator', (tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: SplashPage(),
+        ),
+      );
+
+      await tester.pump(Duration.zero);
+
+      final welcomeTextFinder = find.byType(
+        LinearProgressIndicator,
+      );
+
+      expect(welcomeTextFinder, findsOneWidget);
+    });
+  });
+}
