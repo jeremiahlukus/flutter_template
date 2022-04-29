a55416384a0ff8a8fdff67f650078432434e467d
Victor Eronmosele <victoreronmosele@gmail.com>
2022-04-29 13:33:47 +0100

    Add test for BackendHeadersCache

--- lib/backend/core/infrastructure/user_local_service.dart
+++ lib/backend/core/infrastructure/user_local_service.dart
@@ -1,20 +1,24 @@
 // Package imports:
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:sembast/sembast.dart';
 
 // Project imports:
 import 'package:flutter_template/backend/core/infrastructure/user_dto.dart';
 import 'package:flutter_template/core/infrastructure/sembast_database.dart';
 
-const key = 'user';
-
 class UserLocalService {
   final SembastDatabase _sembastDatabase;
-  final _store = stringMapStoreFactory.store('headers');
+
+  @visibleForTesting
+  static final store = stringMapStoreFactory.store('headers');
+
+  @visibleForTesting
+  static const key = 'user';
 
   UserLocalService(this._sembastDatabase);
 
   Future<void> saveUser(UserDTO dto) async {
-    await _store.record(key).put(
+    await store.record(key).put(
           _sembastDatabase.instance,
           dto.toJson(),
         );
@@ -22,11 +26,11 @@ class UserLocalService {
 
   Future<UserDTO> getUser() async {
     // ignore: cast_nullable_to_non_nullable
-    final json = await _store.record(key).get(_sembastDatabase.instance) as Map<String, dynamic>;
+    final json = await store.record(key).get(_sembastDatabase.instance) as Map<String, dynamic>;
     return UserDTO.fromJson(json);
   }
 
   Future<void> deleteUser() async {
-    await _store.record(key).delete(_sembastDatabase.instance);
+    await store.record(key).delete(_sembastDatabase.instance);
   }
 }
