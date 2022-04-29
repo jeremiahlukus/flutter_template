116d48abffadf9f8b2f5c72967f8a017901fb06f
Victor Eronmosele <victoreronmosele@gmail.com>
2022-04-29 15:16:11 +0100

    Victor Eronmosele | Format code

--- test/backend/core/infrastructure/user_remote_service_test.dart
+++ test/backend/core/infrastructure/user_remote_service_test.dart
@@ -56,7 +56,8 @@ void main() {
 
         when(() => mockDio.getUri<dynamic>(any(), options: any(named: 'options'))).thenAnswer(
           (invocation) => Future.value(
-              Response<dynamic>(requestOptions: RequestOptions(path: ''), statusCode: 200, data: mockData),),
+            Response<dynamic>(requestOptions: RequestOptions(path: ''), statusCode: 200, data: mockData),
+          ),
         );
 
         when(() => mockBackendHeadersCache.getHeaders(any())).thenAnswer(
@@ -118,9 +119,12 @@ void main() {
         final Dio mockDio = MockDio();
         final BackendHeadersCache mockBackendHeadersCache = MockBackendHeadersCache();
 
-        when(() => mockDio.getUri<dynamic>(any(), options: any(named: 'options'))).thenThrow(DioError(
+        when(() => mockDio.getUri<dynamic>(any(), options: any(named: 'options'))).thenThrow(
+          DioError(
             requestOptions: RequestOptions(path: ''),
-            response: Response<dynamic>(requestOptions: RequestOptions(path: '')),),);
+            response: Response<dynamic>(requestOptions: RequestOptions(path: '')),
+          ),
+        );
 
         when(() => mockBackendHeadersCache.getHeaders(any())).thenAnswer(
           (invocation) => Future.value(),
