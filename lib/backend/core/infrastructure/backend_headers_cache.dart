cb0636994f063448ef4b09aabad9a52c49a86645
Victor Eronmosele <victoreronmosele@gmail.com>
2022-04-29 15:05:02 +0100

    Add test for BackendHeadersCache

--- lib/backend/core/infrastructure/backend_headers_cache.dart
+++ lib/backend/core/infrastructure/backend_headers_cache.dart
@@ -1,4 +1,5 @@
 // Package imports:
+import 'package:flutter/material.dart';
 import 'package:sembast/sembast.dart';
 
 // Project imports:
@@ -7,23 +8,24 @@ import 'package:flutter_template/core/infrastructure/sembast_database.dart';
 
 class BackendHeadersCache {
   final SembastDatabase _sembastDatabase;
-  final _store = stringMapStoreFactory.store('headers');
+  @visibleForTesting
+  static final store = stringMapStoreFactory.store('headers');
 
   BackendHeadersCache(this._sembastDatabase);
 
   Future<void> saveHeaders(Uri uri, BackendHeaders headers) async {
-    await _store.record(uri.toString()).put(
+    await store.record(uri.toString()).put(
           _sembastDatabase.instance,
           headers.toJson(),
         );
   }
 
   Future<BackendHeaders?> getHeaders(Uri uri) async {
-    final json = await _store.record(uri.toString()).get(_sembastDatabase.instance);
+    final json = await store.record(uri.toString()).get(_sembastDatabase.instance);
     return json == null ? null : BackendHeaders.fromJson(json);
   }
 
   Future<void> deleteHeaders(Uri uri) async {
-    await _store.record(uri.toString()).delete(_sembastDatabase.instance);
+    await store.record(uri.toString()).delete(_sembastDatabase.instance);
   }
 }
