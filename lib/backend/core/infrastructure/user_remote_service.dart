52f738753b42da8691fb878bc192ebe565fb0b1b
Jeremiah Parrack <jeremiahlukus1@gmail.com>
2022-04-30 10:38:05 -0400

    Jeremiah-Parrack | update tests

--- lib/backend/core/infrastructure/user_remote_service.dart
+++ lib/backend/core/infrastructure/user_remote_service.dart
@@ -17,7 +17,7 @@ class UserRemoteService {
 
   Future<RemoteResponse<UserDTO>> getUserDetails() async {
     final requestUri = Uri.http(
-      backendBaseUrl(),
+      BackendConstants().backendBaseUrl(),
       'api/v1/me',
     );
 
