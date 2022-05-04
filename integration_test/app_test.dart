b7680a7032b76950f1c52622d0ee50734043a168
Victor Eronmosele <victoreronmosele@gmail.com>
2022-05-04 23:28:13 +0100

    Victor Eronmosele | Add integration test

---
+++ integration_test/app_test.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+import 'package:flutter_template/auth/presentation/authorization_page.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+import '../test/auth/presentation/authorization_page_test.dart';
+
+class MockNavigatorObserver extends Mock implements NavigatorObserver {}
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('WebViewExample', () {
+    testWidgets('fill the email and password text fields', (WidgetTester tester) async {
+      final mockObserver = MockNavigatorObserver();
+
+      final OnAuthorizationCodeRedirectAttemptCallback mockOnAuthorizationCodeRedirectAttemptCallback =
+          MockOnAuthorizationCodeRedirectAttemptCallback();
+
+      when(mockOnAuthorizationCodeRedirectAttemptCallback.call).thenReturn((_) {});
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: AuthorizationPage(
+            authorizationUrl: Uri(scheme: ''),
+            onAuthorizationCodeRedirectAttempt: mockOnAuthorizationCodeRedirectAttemptCallback(),
+          ),
+          navigatorObservers: [mockObserver],
+        ),
+      );
+      await tester.pumpAndSettle();
+
+      expect(find.byType(WebView), findsOneWidget);
+    });
+  });
+}
